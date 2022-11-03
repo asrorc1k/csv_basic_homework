df795618b57a0f3824e123e1964d46f050cd8a78
asrorc1k <asrornortojiev@gmail.com>
2022-11-03 14:59:08 +0300

    0002

--- find_number_of_rows.py
+++ find_number_of_rows.py
@@ -6,6 +6,10 @@ def find_number_of_rows(data):
     Return:
         int: Number of rows.
     """
-    return
-
+reader = csv.reader(f)
+a=0
+for row in reader:
+    a+=1
+    return a
 # Read the csv file
+f = open('data.csv').read().split()
